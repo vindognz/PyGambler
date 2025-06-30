d4fac997ac36e3c9b0197062f4f8b8dc4929249f
Vincent <vindognz@gmail.com>
2025-06-30 14:22:06 +1200

    promises, optimization

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     gambleButton.addEventListener('click', () => {
         output.value = input.value;
+        rollAll();
     });
 })
 
@@ -12,23 +13,39 @@ const icon_width = 79,
       icon_height = 79,
       num_icons = 9,
       time_per_icon = 100,
-      indexes = [0, 0, 0];
+      indexes = [0, 0, 0],
+      icon_map = ["banana", "seven", "cherry", "plum", "orange", "bell", "bar", "lemon", "melon"];
 
 const roll = (reel, offset=0) => {
     const delta = (2 + offset) * num_icons + Math.round(Math.random() * num_icons);
     const style = getComputedStyle(reel),
-            backgroundPositionY = parseFloat(style["background-position-y"]);
+            backgroundPositionY = parseFloat(style["background-position-y"]),
+            targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
+            normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
 
-    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms`;
-    reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+    return new Promise((resolve, reject) => {
+        reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms`;
+        reel.style.backgroundPositionY = `${targetBackgroundPositionY}px`;
+
+        setTimeout(() => {
+            reel.style.transition = `none`;
+            reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`
+            resolve(delta % num_icons)
+        }, 8 + delta * time_per_icon);
+    });
 }
 
 function rollAll() {
     const reelsList = document.querySelectorAll('.slots > .reel');
-    [...reelsList].map((reel, i) => {
-        console.log("uppdated");
-        roll(reel, i);
-    })
+    Promise
+        .all( [... reelsList].map((reel, i) => roll(reel, i)))
+        .then((deltas) => {
+
+            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta) % num_icons)
+
+            indexes.map((index) => { console.log(icon_map[index]) })
+
+        })
 }
 
 rollAll();
